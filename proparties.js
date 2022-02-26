96bc52fd2c85a8e2a6ec63909f065667376f1fe4
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-02-26 13:03:11 +0600

    Object keys and values extracting with Object.keys and Object.values methods

---
+++ proparties.js
@@ -0,0 +1,11 @@
+const bottle = {
+    color: 'White',
+    hold: 'Water',
+    price: 100,
+    isCleaned: true
+};
+
+const keys = Object.keys(bottle);
+const values = Object.values(bottle);
+console.log(keys);
+console.log(values);
