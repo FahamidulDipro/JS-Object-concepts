35a9193eb8bae331d18ce0391962f1478727238b
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-02-26 12:40:17 +0600

    Different methods of declaring objects added

--- object-create.js
+++ object-create.js
@@ -1,5 +1,22 @@
-//Using object litaral
+// 1.Using object litaral
 const student = {
     name: 'Khalid Hasan',
     job: 'Programmer'
 }
+
+// 2.Object Constructor
+// const person = new Object();
+// console.log(person);
+
+//
+const human = Object.create(student);
+console.log(human.job);
+
+class People {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const person = new People('Kamran', 28);
+console.log(person);
