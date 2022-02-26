806383a6ded7fe3f7e97ca9a588ad261e88a007e
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-02-26 13:06:49 +0600

    getting key and value pairs using Object.entries method

--- proparties.js
+++ proparties.js
@@ -5,7 +5,12 @@ const bottle = {
     isCleaned: true
 };
 
+//Getting All Proparties names
 const keys = Object.keys(bottle);
+//Getting All values 
 const values = Object.values(bottle);
+//Getting Key and Value pairs
+const entities = Object.entries(bottle);
 console.log(keys);
 console.log(values);
+console.log(entities);
